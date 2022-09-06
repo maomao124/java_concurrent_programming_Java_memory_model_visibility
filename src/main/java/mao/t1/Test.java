e7495c9013d7b89971c6ce77b571049bd3e7ed3c
mao <1296193245@qq.com>
2022-09-06 13:01:05 +0800

    java_concurrent_programming_Java_memory_model_visibility

---
+++ src/main/java/mao/t1/Test.java
@@ -0,0 +1,63 @@
+package mao.t1;
+
+import java.util.Date;
+
+/**
+ * Project name(项目名称)：java并发编程_Java内存模型_可见性
+ * Package(包名): mao.t1
+ * Class(类名): Test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/9/6
+ * Time(创建时间)： 12:43
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Test
+{
+    private static boolean run = true;
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (run)
+                {
+                    //运行
+                }
+                System.out.println("运行结束");
+            }
+        }, "t1").start();
+
+        Thread thread = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (true)
+                {
+                    System.out.println("------>" + new Date());
+                    try
+                    {
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "t2");
+        thread.setDaemon(true);
+        thread.start();
+
+        Thread.sleep(1500);
+        System.out.println("开始停止");
+        run = false; //并不会停止运行
+    }
+}
